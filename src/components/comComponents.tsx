4c9d1de939327c8e45f9038090f63151b47e1de3
Peace-Ishimwe <peaceishimwem@gmail.com>
2023-05-25 21:43:37 +0200

    login page

---
+++ src/components/comComponents.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, Text , TextInput } from 'react-native'
+
+interface HeaderProps {
+    mainTitle: String;
+    subTitle: String;
+}
+
+export const HeaderComponents = (props: HeaderProps) => {
+    return (
+        <>
+            <Text className='text-center text-7xl font-bold text-white'>{props.mainTitle}</Text>
+            <Text className='text-text-color text-center text-2xl'>{props.subTitle}</Text>
+        </>
+    )
+}
+
+interface InputLabelComponentProps {
+    title: string;
+}
+
+export const InputLabelComponent = (props: InputLabelComponentProps) => {
+    return (
+        <>
+            <Text className='text-text-color text-2xl'>{props.title}</Text>
+        </>
+    )
+}
+
+export const InputElement = () => {
+    return (
+        <>
+            <TextInput></TextInput>
+        </>
+    )
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { View , Text , Image  , StyleSheet , ImageBackground} from 'react-native'
+import { HeaderComponents , InputLabelComponent } from '../components/comComponents'
 
 const Login = () => {
 return (
     <ImageBackground className="flex justify-center h-[100vh] items-center" source={require('../../assets/Login_Screen.png')}>
       <View className='w-9/12'>
-        <Text className='text-center text-7xl font-bold text-white'>Welcome Back!</Text>
-        <Text className='text-text-color text-center text-2xl'>welcome back we missed you</Text>
+        <HeaderComponents mainTitle={"Welcome Back!"} subTitle={"welcome back we missed you"} />
       </View>
       <View className='w-8/12 mt-10'>
-        <Text className='text-text-color text-2xl'>Username</Text>
+        <InputLabelComponent title={"Username"} />
       </View>
     </ImageBackground>
   )
