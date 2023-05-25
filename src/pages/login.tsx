a856445e7b7bf6cfbe18b801db371f8e6ca13d2e
Peace-Ishimwe <peaceishimwem@gmail.com>
2023-05-25 20:30:34 +0200

    login screen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,13 +3,13 @@ import { View , Text , Image  , StyleSheet , ImageBackground} from 'react-native
 
 const Login = () => {
 return (
-    <ImageBackground className="flex justify-center h-[100vh] items-center" source={require('../../assets/Login_Screen.png')} style={styles.background}>
+    <ImageBackground className="flex justify-center h-[100vh] items-center" source={require('../../assets/Login_Screen.png')}>
       <View className='w-9/12'>
         <Text className='text-center text-7xl font-bold text-white'>Welcome Back!</Text>
         <Text className='text-text-color text-center text-2xl'>welcome back we missed you</Text>
       </View>
-      <View className='w-8/12 mt-3'>
-        <Text className='text-text-color'>Username</Text>
+      <View className='w-8/12 mt-10'>
+        <Text className='text-text-color text-2xl'>Username</Text>
       </View>
     </ImageBackground>
   )
